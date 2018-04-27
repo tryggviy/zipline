a7bb9bb6ce71cb8049c35df51cb5f504149815b0
Richard Frank <rich@quantopian.com>
2018-04-27 11:00:22 -0400

    TST: Skip pipeline categorical tests under new pandas
    Categoricals changed in 0.19

--- zipline/utils/pandas_utils.py
+++ zipline/utils/pandas_utils.py
@@ -13,8 +13,11 @@ from distutils.version import StrictVersion
 from trading_calendars.utils.pandas_utils import days_at_time  # noqa: reexport
 
 pandas_version = StrictVersion(pd.__version__)
+new_pandas = pandas_version >= StrictVersion('0.19')
+skip_pipeline_new_pandas = \
+    'Pipeline categoricals are not yet compatible with pandas >=0.19'
 
-if pandas_version >= StrictVersion('0.20.0'):
+if pandas_version >= StrictVersion('0.20'):
     from pandas.core.tools.datetimes import normalize_date  # noqa
 else:
     from pandas.tseries.tools import normalize_date  # noqa
